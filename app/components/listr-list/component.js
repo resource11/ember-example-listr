effb0a8f269e6f922f834086e7254c525108d175
Jeffrey Horn <jeffh@generalassemb.ly>
2015-12-09 14:04:21 -0500

    Persist hidden state of list component

--- app/components/listr-list/component.js
+++ app/components/listr-list/component.js
@@ -3,11 +3,18 @@ import Ember from 'ember';
 export default Ember.Component.extend({
   tagName: 'ul',
   classNames: ['listr-list'],
-  hidden: false,
+  hidden: Ember.computed.alias('list.hidden'),
   classNameBindings: ['hidden:listr-list--hidden'],
+  store: Ember.inject.service(),
   actions: {
     toggleList: function () {
-      this.toggleProperty('hidden');
+      var id = this.get('list.id');
+      var store = this.get('store');
+
+      store.findRecord('list', id).then(function (list) {
+        list.toggleProperty('hidden');
+        list.save();
+      });
     }
   }
 });
--- app/mirage/config.js
+++ app/mirage/config.js
@@ -2,6 +2,9 @@ export default function() {
   this.namespace = 'api';
 
   this.get('/lists');
+  this.get('/lists/:id');
+  this.put('/lists/:id');
+
   this.get('/items/:id');
   this.put('/items/:id');
 }
